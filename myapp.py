01073c42412b5d93e187b47120a7e76e1e01a50b
MathanKumar14 <51161967+MathanKumar14@users.noreply.github.com>
2023-03-11 14:07:30 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -8,23 +8,33 @@ class MyApp:
         self.media_info = self.client.media_info(self.media_pk).dict()
         self.media_type = self.media_info['media_type']
         self.product_type = self.media_info['product_type']
+        
 
     def get_link(self):
         media_type = self.media_type
         media_info = self.media_info
+        product_type = self.product_type
 
         if media_type == 1:
-            return media_info['thumbnail_url']
+            return {'photo':media_info['thumbnail_url']}
         elif media_type == 2:
-            return media_info['video_url']
-        elif media_type == 8:
-            urls = []
-            resources = media_info['resources']
-            for post in resources:
-                loc_type = post['media_type']
-                if loc_type == 1:
-                    urls.append(post['thumbnail_url'])
-                elif loc_type == 2:
-                    urls.append(post['video_url'])
+            if product_type == 'feed':
+                return {'video':media_info['video_url']}
+            elif product_type == 'igtv':
+                return {'igtv':media_info['video_url']}
+            elif product_type == 'clips':
+                return {'reels':media_info['video_url']}
 
-            return urls
+        elif media_type == 8:
+            photos = []
+            videos = []
+            urls = {}
+            resources = media_info['resources']
+
+            for post in resources:
+                if post['media_type'] == 1:
+                    photos.append(post['thumbnail_url'])
+                elif post['media_type'] == 2:
+                    videos.append(post['video_url'])
+            return {'photo':photos,'videos':videos}
+
